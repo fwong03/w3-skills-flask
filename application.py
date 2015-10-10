c3fbfdbf830316fbff9ff3e47ddbb59b60be5dcf
fwong <fswong@gmail.com>
2015-10-10 16:52:21 -0700

    Applicatioin response page mostly working

--- application.py
+++ application.py
@@ -5,7 +5,7 @@ Provides web interface for applying for a job.
 """
 
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -15,10 +15,32 @@ def index_page():
     """Show an index page."""
     return render_template("index.html")
 
+
 @app.route("/application-form")
 def application_page():
     """Show application form page."""
     return render_template("application-form.html")
+
+
+@app.route("/application-response", methods=["POST"])
+def application_confirmation():
+    """Show application submission confirmation"""
+    jobs = {"software": "Software Engineer",
+                "qa": "QA Engineer",
+                "product": "Product Manager"
+            }
+
+    firstname = request.form.get("firstname")
+    lastname = request.form.get("lastname")
+    salary = int(request.form.get("salary"))
+    job = jobs[request.form.get("job")]
+
+    return "first name: %s, last name: %s, salary: %d, job: %s" % (firstname,
+        lastname, salary, job)
+
+    # return render_template("application-response.html",
+    #                         fname=firstname, lname=lastname,
+    #                         money=salary, position=job)
 
 
 if __name__ == "__main__":
